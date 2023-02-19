17b678a139d2ed92bd8f2cbf8974fabeabf07a13
dmitry umrikhin <u-dv@yandex.ru>
2023-02-19 14:17:07 +0300

    task 4

--- hw oop-2.py
+++ hw oop-2.py
@@ -87,33 +87,71 @@ class Reviewer(Mentor):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Ruoy', 'Eman', 'male')
 best_student.courses_in_progress += ['Python', 'Git']
 best_student.finished_courses += ['Введение в программирование']
 
+good_student = Student('Ivan', 'Ivanov', 'male')
+good_student.courses_in_progress += ['Python', 'Git']
+good_student.finished_courses += ['Введение в программирование']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
  
+good_reviewer = Reviewer('Some', 'Buddy')
+good_reviewer.courses_attached += ['Python']
+
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
 
 good_lecturer = Lecturer('Some', 'Buddy')
 good_lecturer.courses_attached += ['Python']
 
-
-
-
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-
 best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 best_student.rate_lecturer(good_lecturer, 'Python', 9)
 
-cool_reviewer.rate_hw(best_student, 'Python', 9)
+good_student.rate_lecturer(cool_lecturer, 'Python', 10)
+good_student.rate_lecturer(good_lecturer, 'Python', 9)
 
-# print(best_student)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(good_student, 'Python', 9)
 
-print(good_lecturer < cool_lecturer)
+good_reviewer.rate_hw(best_student, 'Python', 10)
+good_reviewer.rate_hw(good_student, 'Python', 9)
+
+student_list = [best_student, good_student]
+lecturer_list = [cool_lecturer, good_lecturer]
+
+def get_average_student_rating(list, course):
+    rates_number = 0
+    rates_sum = 0
+    for student in list:
+        for key, value in student.grades.items():
+            if key == course:
+                rates_number += len(value)
+                rates_sum += sum(value)
+    return(rates_sum / rates_number)
+
+def get_average_lecturer_rating(list, course):
+    rates_number = 0
+    rates_sum = 0
+    for lecturer in list:
+        for key, value in lecturer.grades.items():
+            if key == course:
+                rates_number += len(value)
+                rates_sum += sum(value)
+    return(rates_sum / rates_number)
+
+
+course = 'Python'
+
+print(f"Cредняя оценка студентов на курсе {course} :" \
+    f"{get_average_student_rating(student_list, course)}")
+print(f"Cредняя оценка лекторов на курсе {course} :" \
+    f"{get_average_lecturer_rating(lecturer_list, course)}")
+
+print(cool_reviewer)
+print(cool_lecturer)
+print(best_student)
+print(best_student > good_student)
+print(cool_lecturer > good_lecturer)
